ff2c3be8611abf8e8457fe0b37b4743803614ce9
Ahmmmyoo <ahmadirfankhan@gmail.com>
2024-12-11 09:35:25 +0500

    hmmmm

--- src/overview.py
+++ src/overview.py
@@ -1,3 +1,15 @@
+# ---------------------------------------------------------
+# Code For Running Outside Docker
+import sys
+import os
+from pathlib import Path
+
+# Add the parent directory of 'src' to the system path
+current_dir = Path(__file__).resolve().parent
+parent_dir = current_dir.parent
+sys.path.insert(0, str(parent_dir))
+## ---------------------------------------------------------
+
 from src.components.overview_page.earthquake_map_app import EarthquakeMapApp
 import streamlit as st # type: ignore
 

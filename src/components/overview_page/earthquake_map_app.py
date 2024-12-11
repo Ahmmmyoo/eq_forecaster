d2371b0e78cf372ea7d151eadf58203a9fa0e6e5
hamza2001-max <ellawliet1007@gmail.com>
2024-12-11 23:10:08 +0500

    Solved a merging issue.

--- src/components/overview_page/earthquake_map_app.py
+++ src/components/overview_page/earthquake_map_app.py
@@ -18,20 +18,6 @@ class EarthquakeMapApp:
         df_table_form.columns = ["Time", "Magnitude", "Latitude", "Longitude", "Place"]
         df_table_form.index = range(1, len(df_table_form) + 1)
         st.dataframe(df_table_form, use_container_width=True)
-        
-    def time_since(self, timestamp):
-        now = datetime.now()
-        diff = now - timestamp
-        # Get the difference in minutes, hours, and days
-        days = diff.days
-        hours, remainder = divmod(diff.seconds, 3600)
-        minutes = remainder // 60
-        if days > 0:
-            return f"{days} day{'s' if days > 1 else ''} {hours} hour{'s' if hours > 1 else ''} {minutes} minute{'s' if minutes > 1 else ''} ago"
-        elif hours > 0:
-            return f"{hours} hour{'s' if hours > 1 else ''} {minutes} minute{'s' if minutes > 1 else ''} ago"
-        else:
-            return f"{minutes} minute{'s' if minutes > 1 else ''} ago"
 
     def run(self):
         time_period, current_continent, min_magnitude = self.sidebar_renderer.render_sidebar()
@@ -41,7 +27,7 @@ class EarthquakeMapApp:
             df = self.data_processor.parse_earthquake_data(data)
             filtered_df = self.data_processor.filter_data(df, min_magnitude)
             st.title("Earthquake Map Viewer")
-            map_type_col, _ = st.columns([2.5, 10])
+            map_type_col, _ = st.columns([1, 10])
             with map_type_col:
                 map_type = st.selectbox(
                     "Select the type of map",
@@ -52,9 +38,7 @@ class EarthquakeMapApp:
                 )
             map_col, color_bar_col = st.columns([0.9, 0.05], vertical_alignment="center")
             with map_col:
-                filtered_df_timeFixedToString = filtered_df.copy()
-                filtered_df_timeFixedToString['time'] = filtered_df_timeFixedToString['time'].apply(self.time_since)
-                self.map_renderer.render_map(filtered_df_timeFixedToString, map_type, current_continent)
+                self.map_renderer.render_map(filtered_df, map_type, current_continent)
             with color_bar_col:
                 self.map_renderer.show_color_bar()
             self.show_data_table(filtered_df)
